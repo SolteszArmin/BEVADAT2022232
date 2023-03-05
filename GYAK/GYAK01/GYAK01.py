7e335d6e5a4f23f7c62c3baa5f0ed8e8ad9fa696
soltaszarmin <solteszarmin@stud.uni-obuda.hu>
2023-03-05 19:54:42 +0100

    1st Lesson Finished

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -1,2 +1,20 @@
-gyak="sajt"
-print(gyak)
+
+printableSentence="Hello World"
+
+
+print(printableSentence)
+
+
+for letter in printableSentence:
+    print(letter)
+
+
+def PrintingMethod(To_Print):
+    for letter in To_Print:
+        print(letter)
+
+
+
+PrintingMethod(printableSentence)
+
+
