63ff34db94abb6816c69b18967be0f52a310fb38
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-03-10 17:29:12 +0100

    Final Finished Homework

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -1,10 +1,4 @@
-
 import numpy as np
-
-
-
-
-
 
 def column_swap(inputArray:np.array)->np.array:
     temp=np.copy(inputArray[:,0])
@@ -12,15 +6,8 @@ def column_swap(inputArray:np.array)->np.array:
     inputArray[:,1]=temp
     return inputArray
 
-
-
-
 def compare_two_array(inputArray1:np.array,inputArray2:np.array)->np.array:
     return np.where(np.equal(inputArray1,inputArray2))
-
-
-
-
 
 def get_array_shape(inputArray:np.array)->str:
    output=""
@@ -32,52 +19,41 @@ def get_array_shape(inputArray:np.array)->str:
 
    return output
 
-
-
 def encode_Y(InputArray:np.array,classCount:int)->np.array:
-    zeroMatrix=np.zeros((classCount,classCount),dtype=int)
-    search=np.where(search>=0)
-    
+    zeroMatrix = np.zeros((len(InputArray),classCount),dtype=int)
+    zeroMatrix[np.arange(len(InputArray)),InputArray]=1
     return zeroMatrix
-encode_Y(np.array([1, 2, 0, 3]), 4)
 
-
-
-
-
-
-
-
+def decode_Y(inputArray:np.array)->np.array:
+    return np.argmax(inputArray,axis=1)
 
 def eval_classification(itemList:list,inputArray:np.array)->str:
     index=np.where(inputArray==np.max(inputArray))
     return itemList[index[0][0]]
 
-
-
-
 def repalce_odd_numbers(inputArray:np.array)->np.array:
     inputArray[(inputArray%2!=0)]=-1
     return inputArray
-
-
-
 
 def replace_by_value(inputArray:np.array,compareValue:int)->np.array:
     inputArray[(inputArray<compareValue)]=-1
     inputArray[(inputArray>=compareValue)]=1
     return inputArray
 
-
-
-
 def array_multi(inputArray:np.array):
     return np.prod(inputArray)
-
-
 
 def array_multi_2d(inputArray:np.array)->np.array:
     return np.multiply.reduce(inputArray,axis=1)
 
+def add_border(inputArray:np.array)->np.array:
+    return np.pad(inputArray,pad_width=1,mode='constant',constant_values=0)
 
+def list_days(startDate:str,endDate:str)->np.array:
+    return np.array(np.arange(startDate+'-01',endDate+'-01',dtype=np.datetime64))
 
+def actual_date()->str:
+    return np.datetime64('today')
+
+def sec_from_1970()->int:
+    return np.datetime64('now','s') - np.datetime64('1970-01-01T00:02:00')
